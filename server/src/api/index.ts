cf99592b8380192e9baa22e1c4423f2f9d54f6a3
Ayushi <ayushi0628@gmail.com>
2024-11-17 19:32:17 +0530

    restructuring

---
+++ server/src/api/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import serverless from 'serverless-http'; 
+import connectDB from '../config/dbConfig.ts';
+import customerRoutes from '../routes/customer.routes.ts';
+import orderRoutes from '../routes/order.routes.ts';
+import subscribeToCustomerEvents from './subscribers/customerSubscriber.ts';
+import subscribeToOrderEvents from './subscribers/orderSubscriber.ts';
+import segmentRoutes from '../routes/segment.routes.ts';
+import campaignRoutes from '../routes/campaign.routes.ts';
+import subscribeToSegmentEvents from './subscribers/segmentSubscriber.ts';
+import { subscribeToMessageEvents } from './subscribers/messageSubscriber.ts';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5001',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Middleware
+app.use(express.json()); // For parsing JSON requests
+
+// Connect to Database
+connectDB();
+
+subscribeToCustomerEvents();
+subscribeToOrderEvents();
+subscribeToSegmentEvents();
+subscribeToMessageEvents();
+
+// Define routes
+app.use(customerRoutes);
+app.use(orderRoutes);
+app.use(segmentRoutes);
+app.use(campaignRoutes);
+
+module.exports.handler = serverless(app); // Export as serverless function
